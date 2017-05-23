ed42fa550226cb74e9f2be51687cfbfa85775092
Heather Walker <cookiesncream@ladle.net>
2017-05-23 15:13:12 -0700

    contact form created, grabs info, goes to success page

--- server.js
+++ server.js
@@ -10,11 +10,17 @@ app.use(express.static(path.join(__dirname, "./static")));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
 // use body-parser
-app.use(bodyParser.urlencoded());
+// app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({extended: true}));
 // root route
 app.get('/', function(req, res){
 	res.render('index');
 });
+// contact route
+app.post('/contact', function(req, res){
+	console.log('name=' + req.body.name + ' email=' + req.body.email + ' phone=' + req.body.phone + ' message=' + req.body.message);
+	res.render('success')
+})
 
 // listen on port 8000
 app.listen(8000, function(){
